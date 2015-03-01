a70516e59889ae6106f0313121ff6e8d7e01ee46
Archangel7 <michaelcx2760@gmail.com>
2015-03-01 15:11:52 -0500

    Create interface.c

---
+++ interface.c
@@ -0,0 +1,19 @@
+/****************************************
+*This program is a basic user-interface
+* for a virtual CPU 
+*
+* Creation Date: january 30, 2015
+* Author: Michael Cox
+*
+*****************************************/
+#include "input.h"
+#include "functionDef.h"
+#include "functionBody.h"
+#define memsize 16384
+
+unsigned char MEMORY[memsize]; // 16KBytes for computer memory
+
+int main()
+{
+	input(&MEMORY,sizeof MEMORY);
+}
