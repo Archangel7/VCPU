9c37c447fe54e33ab730a7a711e78f91c9f78d69
Archangel7 <michaelcx2760@gmail.com>
2015-03-29 19:04:20 -0400

    Update functionDef.h

--- functionDef.h
+++ functionDef.h
@@ -1,5 +1,6 @@
 /***********************************
 * File: functionDef.h
+* Author: Michael Cox
 * Date Created: January 30th, 2015
 *
 ***********************************/
@@ -14,3 +15,13 @@ void memModify(void *memory);
 unsigned getinput(char *format);
 void resetRegisters();
 void trace(void *);
+void clearRegisters();
+unsigned maskvalues(unsigned short valueToMask, int start, int finish);
+void clearFlags();
+void reset();
+void split();
+void displayRegisters();
+void fetch(void *memory);
+void exec(void *);
+void execute(unsigned short*);
+int iscarry(unsigned long op1, unsigned long op2, unsigned c);
